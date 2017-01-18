decde17ba6c893a02155c76adf41195b20bcca87
nagabhushanamn <nagabhushanamn@gmail.com>
2017-01-18 17:42:05 +0530

    let & const keywords with block scope

---
+++ ES6-learn/1-let-const-keywords.js
@@ -0,0 +1,48 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+
+//console.log(productId);
+//let productId=12;
+
+
+//var productId=12;
+//
+//{
+//	let productId=14;
+//}
+//
+//console.log(productId);
+
+
+//var i=100;
+//for(let i=0;i<10;i++){
+//	
+//}
+//console.log(i);
+
+//----------------------
+
+
+//var PI=3.14;
+//Object.defineProperty(window, "PI", {writable:false})
+
+
+//const PI=3.14;
+
+//const person={name:"Nag"};
+//person={name:'Ria'};
+
+
+//-----------------------------
+
+{
+	const V1=10;
+}
+
+console.log(V1);
+
+
+
+

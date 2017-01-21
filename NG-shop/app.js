b6624f493695a6c451bed0e4a094c62827e4a4d2
nagabhushanamn <nagabhushanamn@gmail.com>
2017-01-21 10:55:25 +0530

    ng sample

---
+++ NG-shop/app.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+
+var shopMod=angular.module('shop', []);
+
+
+// Model ( data ) ( usually loaded from server-side )
+var item={
+		name:'Laptop',
+		price:198000,
+		description:'New Mac Pro'
+};
+
+
